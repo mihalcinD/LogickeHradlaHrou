4040570eda3dbe51135ccf36f14c704ccc9de850
mBerdis <marosberdis@gmail.com>
2021-03-11 09:55:12 +0100

    Empty project v2

--- src/steakoverflow/Main.java
+++ src/steakoverflow/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package steakoverflow;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
         primaryStage.setTitle("Hello Matej");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
